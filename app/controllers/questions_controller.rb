b2dde8c6af37323a1fcab52c0b038d48c6e44b1f
Frank Gerngross <f.gerngross@googlemail.com>
2022-07-05 20:45:25 +0200

    added all answers and styles

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,16 @@ class QuestionsController < ApplicationController
 
   def ask
   end
+
+  def answer
+    stupid_question = params[:question]
+
+    if stupid_question == "I am going to work"
+      @answer = "Great!"
+    elsif stupid_question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
